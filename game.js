2281eb69bff742c6cc371b22956ea8269bd2e9dd
maria <marcavmer@alu.edu.gva.es>
2024-07-18 14:22:07 +0200

    mario move right-left

--- game.js
+++ game.js
@@ -18,7 +18,6 @@ new Phaser.Game(config);
 
 function preload() {
   this.load.image("cloud1", "assets/scenery/overworld/cloud1.png");
-
   this.load.image("floorbricks", "assets/scenery/overworld/floorbricks.png");
   this.load.spritesheet(
     "mario",
@@ -28,15 +27,40 @@ function preload() {
 }
 
 function create() {
-    //x,y,id
+  //x,y,id
   this.add.image(100, 50, "cloud1").setScale(0.15).setOrigin(0, 0);
-    this.add.tileSprite(0, config.height, config.width, 32, 'floorbricks')
-    .setOrigin(0,1) //es una textura, se puede expandir
-  this.add.sprite(50, 210, "mario").setOrigin(0, 1);
+  this.add
+    .tileSprite(0, config.height, config.width, 32, "floorbricks")
+    .setOrigin(0, 1); //es una textura, se puede expandir
+  //guardar el mario para que pueda mover -->
+  this.mario = this.add.sprite(50, 210, "mario").setOrigin(0, 1);
 
   //crear las teclas para poder visualizarlas en update
-  this.keys = this.input.keyboard.createCursorKeys()
+  this.keys = this.input.keyboard.createCursorKeys();
 
+  //crear animación
+  this.anims.create({
+    key: "mario-walk",
+    frames: this.anims.generateFrameNumbers("mario", { start: 1, end: 3 }),
+    repeat: -1, //infinito -1
+  });
+
+  this.anims.create({
+    key: 'mario-idle',
+    frames: [{key: 'mario', frame: 0}]
+  })
 }
 
-function update() {}
+function update() {
+  if (this.keys.left.isDown) {
+    this.mario.x -= 2;
+    this.mario.anims.play("mario-walk", true);
+  } else if (this.keys.right.isDown) {
+    this.mario.x += 2;
+    this.mario.anims.play("mario-walk", true);
+    
+  } else {
+    this.mario.anims.stop()
+    this.mario.setFrame(0) //volver al frame 0 del mario
+  }
+}
