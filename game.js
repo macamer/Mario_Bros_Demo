7cbb432cafacfddc1f564310183e319c11eaf548
maria <marcavmer@alu.edu.gva.es>
2024-07-18 13:48:28 +0200

    add cloud and mario

--- game.js
+++ game.js
@@ -3,7 +3,7 @@ const config = {
   type: Phaser.AUTO, //tipo de renderizado webgl, canvas or auto
   width: 256,
   height: 244,
-  backgroundColor: "#ffffff",
+  backgroundColor: "#049cd8",
   parent: "game", //donde se va renderizar
 
   scene: {
@@ -17,11 +17,26 @@ const config = {
 new Phaser.Game(config)
 
 function preload() {
-    console.log('preload');
+    this.load.image(
+        'cloud1',
+        'assets/scenery/overworld/cloud1.png'
+    )
+    
+    this.load.spritesheet(
+        'mario',
+        'assets/entities/mario.png',
+        {frameWidth: 14} //lo que ocupa el año del primer mario
+    )
 }
 
 function create() {
-    console.log('create')
+    this.add.image(100,50,'cloud1')
+    .setScale(0.15)
+    .setOrigin(0,0)
+
+    this.add.sprite(50,200, 'mario')
+    .setOrigin(0,1)
+
 }
 
 function update() {}
