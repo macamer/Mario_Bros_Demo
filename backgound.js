3a68a308d608290b398fed516ad20be76dffb103
maria <marcavmer@alu.edu.gva.es>
2024-07-22 16:30:29 +0200

    add mountain1

--- backgound.js
+++ backgound.js
@@ -1,5 +1,5 @@
 export const createBackground = (game) => {
-
+//vertical, horizontal, clave
   game.add.image(150, 50, "cloud1")
     .setScale(0.15)
     .setOrigin(0, 0)
@@ -8,11 +8,15 @@ export const createBackground = (game) => {
     .setOrigin(0, 0)
   game.add.image(0, 240, "mountain2")
     .setOrigin(0, 1)
+  game.add.image(300, 212, "mountain1")
+    .setOrigin(0, 1)
   game.add.image(250, 212, "bush1")
     .setScale(0.5)
     .setOrigin(0, 1)
   game.add.image(25, 40, 'logo')
     .setScale(0.4)
     .setOrigin(0,0)
+
+
 
 }
