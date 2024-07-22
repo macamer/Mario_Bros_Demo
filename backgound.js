52901d866c758c37b008817a5be37156eed71525
maria <marcavmer@alu.edu.gva.es>
2024-07-22 15:56:01 +0200

    split background on new js

---
+++ backgound.js
@@ -0,0 +1,18 @@
+export const createBackground = (game) => {
+
+  game.add.image(150, 50, "cloud1")
+    .setScale(0.15)
+    .setOrigin(0, 0)
+  game.add.image(70, 100, "cloud2")
+    .setScale(0.15)
+    .setOrigin(0, 0)
+  game.add.image(0, 240, "mountain2")
+    .setOrigin(0, 1)
+  game.add.image(250, 212, "bush1")
+    .setScale(0.5)
+    .setOrigin(0, 1)
+  game.add.image(25, 40, 'logo')
+    .setScale(0.4)
+    .setOrigin(0,0)
+
+}
